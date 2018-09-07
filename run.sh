251a9bfb4f8a4439fbf5c6aad31c6c35c01e2cdd
Martin Brien <Martin Brien>
2018-09-07 16:49:39 +0000

    dfsfd

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-time ansible-playbook -i mbrien, --vault-password-file=~/vault-password dev.yml -k --extra-vars "hostname=mbrien username=martin archive=tarsnap" 
+time ansible-playbook -i mbrien, --vault-password-file=~/vault-password vps-setup.yml -k --extra-vars "hostname=mbrien username=martin archive=tarsnap" 
 
